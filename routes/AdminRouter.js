c265b47dabba7a2a31418143db27767cd4348e7a
Yash Khatri <yash.khatri@minddeft.net>
2023-05-25 18:54:14 +0530

    admin

--- routes/AdminRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.post('/admin-signup',(req,res) => {
-  res.status(200).json({msg: 'hello'})
-})
-
-module.exports = router;
