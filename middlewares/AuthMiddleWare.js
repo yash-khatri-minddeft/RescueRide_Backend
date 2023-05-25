e6d623dbbcc5894612df339230d369b01fc4402e
ParthMinddeft <parth.talsaniya@minddeft.net>
2023-05-25 19:16:20 +0530

    authmiddleware

---
+++ middlewares/AuthMiddleWare.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = async(req,res,next) => {
+    try {
+        const token = req.headers['authorization'].split('')[1];
+        console.log('token',req.headers)
+        jwt.verify(token,process.env.JWT_SECRETKEY,(err,decode) => {
+            if (err) {
+                return res.status(200).send({message:'Auth Failed',success:false})
+            }
+            else
+            {
+                req.body.userId = decode.id;
+                next();
+            }
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(401).send({message:'Auth Failed',success:false})
+    }
+}
