f59f3eb43f7be7aab716809fce3b8ebad77fcd77
ParthMinddeft <parth.talsaniya@minddeft.net>
2023-05-25 19:16:20 +0530

    authmiddleware

--- routes/AdminRouter.js
+++ routes/AdminRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const {adminRegisterController,adminLoginController,authController} = require('../controllers/adminCtrl')
+const authmiddleware = require('../middlewares/AuthMiddleWare')
 
 const router = express.Router();
 
-router.post('/admin-signup',adminRegisterController)
+router.post('/admin-signup',authmiddleware,adminRegisterController)
 
-router.post('/admin-login',adminLoginController)
+router.post('/admin-login',authmiddleware,adminLoginController)
 
-router.post('/getUserData',authController)
+router.post('/getUserData',authmiddleware,authController)
