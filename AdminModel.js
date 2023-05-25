d153c5b36a75ec6b60dade9cb2d0fae498a75f62
Yash Khatri <yash.khatri@minddeft.net>
2023-05-25 16:16:35 +0530

    admin router

---
+++ AdminModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const AdminSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+const admin = new mongoose.model('admin', AdminSchema);
+
+module.exports = admin;
