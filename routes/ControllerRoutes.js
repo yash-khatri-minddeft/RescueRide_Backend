0245721a3cca9eea07b3aa79442be1d98e49f559
Yash Khatri <yash.khatri@minddeft.net>
2023-05-29 12:40:41 +0530

    controller mail

--- routes/ControllerRoutes.js
+++ routes/ControllerRoutes.js
@@ -1,19 +1,21 @@
 const express = require('express')
-const {adminaddController,adminaddambulanceController,adminaddhospitalController,admingethospitalController,admingetallController,admingetambulanceController} = require('../controllers/adminAddController')
+const { adminaddController, adminaddambulanceController, adminaddhospitalController, admingethospitalController, admingetallController, admingetambulanceController, controllerPasswordUpdate } = require('../controllers/adminAddController')
 const authmiddleware = require('../middlewares/AuthMiddleWare')
 
 const router = express.Router();
 
-router.post('/admin-addcontroller',authmiddleware,adminaddController);
+router.post('/admin-addcontroller', authmiddleware, adminaddController);
 
-router.post('/admin-addambulance',authmiddleware,adminaddambulanceController);
+router.post('/admin-addambulance', authmiddleware, adminaddambulanceController);
 
-router.post('/admin-addhospital',authmiddleware,adminaddhospitalController);
+router.post('/admin-addhospital', authmiddleware, adminaddhospitalController);
 
-router.get('/admin-getallcontroller',authmiddleware,admingetallController);
+router.get('/admin-getallcontroller', authmiddleware, admingetallController);
 
-router.get('/admin-gethospital',authmiddleware,admingethospitalController);
+router.get('/admin-gethospital', authmiddleware, admingethospitalController);
 
-router.get('/admin-getambulance',authmiddleware,admingetambulanceController)
+router.get('/admin-getambulance', authmiddleware, admingetambulanceController)
+
+router.post('/ctrl-pass-update', controllerPasswordUpdate)
 
 module.exports = router;
