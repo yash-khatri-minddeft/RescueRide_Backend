9fd99b83446e6529a9f24c7d17d8181277d167a7
ParthMinddeft <parth.talsaniya@minddeft.net>
2023-05-29 13:54:52 +0530

    hospitalpage

--- controllers/adminAddController.js
+++ controllers/adminAddController.js
@@ -1,163 +1,179 @@
-const ambulance = require('../models/AmbulanceModel');
-const controller = require('../models/ControllerModel')
-const bcrypt = require('bcryptjs');
-const hospital = require('../models/HospitalModel');
-const cryptoRandomStringAsync = require('crypto-random-string')
-const jwt = require('jsonwebtoken')
-const { sendPassMail } = require('./MailController')
+const ambulance = require("../models/AmbulanceModel");
+const controller = require("../models/ControllerModel");
+const bcrypt = require("bcryptjs");
+const hospital = require("../models/HospitalModel");
+const cryptoRandomStringAsync = require("crypto-random-string");
+const jwt = require("jsonwebtoken");
+const { sendPassMail } = require("./MailController");
 
 const adminaddController = async (req, res) => {
-	try {
-		const existingController = await controller.findOne({ email: req.body.email });
-		if (existingController) {
-			return res.status(200).send({
-				message: 'Controller Already Exists', success: false
-			})
-		}
-		const password = cryptoRandomStringAsync({ length: 12, type: "url-safe" });
-		console.log(password);
-		const salt = bcrypt.genSaltSync(10);
-		const hashedPassword = bcrypt.hashSync(password, salt);
-		req.body.password = hashedPassword;
-		const newController = new controller(req.body);
-		await newController.save();
-		newController.password = undefined
-		const token = jwt.sign({ id: newController._id }, process.env.JWT_SECRETKEY, {
-			expiresIn: "1d",
-		});
-		sendPassMail(password, req.body.email, token)
-		res.status(201).send({ message: 'Controller Add Successfully', success: true, data: newController })
-	} catch (error) {
-		console.log(error);
-		res.status(500).send({
-			success: false,
-			message: `Add Controller ${error.message}`,
-		})
-	}
-}
+  try {
+    const existingController = await controller.findOne({
+      email: req.body.email,
+    });
+    if (existingController) {
+      return res.status(200).send({
+        message: "Controller Already Exists",
+        success: false,
+      });
+    }
+    const password = cryptoRandomStringAsync({ length: 12, type: "url-safe" });
+    console.log(password);
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+    req.body.password = hashedPassword;
+    const newController = new controller(req.body);
+    await newController.save();
+    newController.password = undefined;
+    const token = jwt.sign(
+      { id: newController._id },
+      process.env.JWT_SECRETKEY,
+      {
+        expiresIn: "1d",
+      }
+    );
+    sendPassMail(password, req.body.email, token);
+    res.status(201).send({
+      message: "Controller Add Successfully",
+      success: true,
+      data: newController,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: `Add Controller ${error.message}`,
+    });
+  }
+};
 
 const adminaddambulanceController = async (req, res) => {
-	try {
-		const newUserAddAmbulance = await ambulance({ ...req.body });
-		await newUserAddAmbulance.save();
-		res.status(201).send({
-			success: true,
-			message: 'Ambulance Added Successfully'
-		})
-	} catch (error) {
-		console.log(error);
-		res.status(500).send({
-			success: false,
-			error,
-			message: 'Error While Adding Ambulance'
-		})
-	}
-}
+  try {
+    const newUserAddAmbulance = await ambulance({ ...req.body });
+    await newUserAddAmbulance.save();
+    res.status(201).send({
+      success: true,
+      message: "Ambulance Added Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error While Adding Ambulance",
+    });
+  }
+};
 
 const adminaddhospitalController = async (req, res) => {
-	try {
-		const newAddHospital = await hospital({ ...req.body });
-		await newAddHospital.save();
-		res.status(201).send({
-			success: true,
-			message: 'Hospital Added Successfully'
-		})
-	} catch (error) {
-		console.log(error);
-		res.status(500).send({
-			success: false,
-			error,
-			message: 'Error While Applying For Doctors'
-		})
-	}
-}
+  try {
+    const newAddHospital = await hospital({ ...req.body });
+    await newAddHospital.save();
+    res.status(201).send({
+      success: true,
+      message: "Hospital Added Successfully",
+      data: newAddHospital,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error While Applying For Doctors",
+    });
+  }
+};
 
 const admingetallController = async (req, res) => {
-	try {
-		const fetchController = await controller.find({}, { password: 0 })
-		res.status(200).send({
-			success: true,
-			message: 'Controller List',
-			data: fetchController
-		})
-
-	} catch (error) {
-		console.log(error);
-		res.status(500).send({
-			success: false,
-			error,
-			message: 'Error While Fetching Controller',
-		})
-	}
-}
+  try {
+    const fetchController = await controller.find({}, { password: 0 });
+    res.status(200).send({
+      success: true,
+      message: "Controller List",
+      data: fetchController,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error While Fetching Controller",
+    });
+  }
+};
 
 const admingethospitalController = async (req, res) => {
-	try {
-		const fetchHospital = await hospital.find({})
-		res.status(200).send({
-			success: true,
-			message: 'Hospital List',
-			data: fetchHospital
-		})
-	} catch (error) {
-		console.log(error);
-		res.status(500).send({
-			success: false,
-			error,
-			message: 'Error While Fetching Hospitals',
-		})
-	}
-}
+  try {
+    const fetchHospital = await hospital.find({});
+    res.status(200).send({
+      success: true,
+      message: "Hospital List",
+      data: fetchHospital,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error While Fetching Hospitals",
+    });
+  }
+};
 
 const admingetambulanceController = async (req, res) => {
-	try {
-		const fetchAmbulance = await ambulance.find({})
-		res.status(200).send({
-			success: true,
-			message: 'Ambulance List',
-			data: fetchAmbulance
-		})
-	} catch (error) {
-		console.log(error);
-		res.status(500).send({
-			success: false,
-			error,
-			message: 'Error While Fetching Ambulance'
-		})
-	}
-}
-
+  try {
+    const fetchAmbulance = await ambulance.find({});
+    res.status(200).send({
+      success: true,
+      message: "Ambulance List",
+      data: fetchAmbulance,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error While Fetching Ambulance",
+    });
+  }
+};
 
 const controllerPasswordUpdate = async (req, res) => {
-	const token = req.body.token;
-	jwt.verify(token, process.env.JWT_SECRETKEY, async (err, decode) => {
-		if (err) {
-			return res.status(200).send({ message: 'Auth Failed', success: false })
-		}
-		else {
-			const password = req.body.password;
-			const cPassword = req.body.cPassword;
-			if (password === cPassword) {
-				const salt = bcrypt.genSaltSync(10);
-				const hashedPassword = bcrypt.hashSync(password, salt);
-				const userId = decode.id;
-				const getCtrl = await controller.findOneAndUpdate({ _id: userId }, { password: hashedPassword });
-				res.status(200).send({ success: true, message: 'Password changed successfully!', token: token })
-			} else {
-				res.status(200).send({ success: false, error: `Password didn't matched!` })
-			}
-		}
-	})
-}
-
-
+  const token = req.body.token;
+  jwt.verify(token, process.env.JWT_SECRETKEY, async (err, decode) => {
+    if (err) {
+      return res.status(200).send({ message: "Auth Failed", success: false });
+    } else {
+      const password = req.body.password;
+      const cPassword = req.body.cPassword;
+      if (password === cPassword) {
+        const salt = bcrypt.genSaltSync(10);
+        const hashedPassword = bcrypt.hashSync(password, salt);
+        const userId = decode.id;
+        const getCtrl = await controller.findOneAndUpdate(
+          { _id: userId },
+          { password: hashedPassword }
+        );
+        res.status(200).send({
+          success: true,
+          message: "Password changed successfully!",
+          token: token,
+        });
+      } else {
+        res
+          .status(200)
+          .send({ success: false, error: `Password didn't matched!` });
+      }
+    }
+  });
+};
 
 module.exports = {
-	adminaddController,
-	adminaddambulanceController,
-	adminaddhospitalController,
-	admingetallController,
-	admingethospitalController,
-	admingetambulanceController,
-	controllerPasswordUpdate
-}
+  adminaddController,
+  adminaddambulanceController,
+  adminaddhospitalController,
+  admingetallController,
+  admingethospitalController,
+  admingetambulanceController,
+  controllerPasswordUpdate,
+};
