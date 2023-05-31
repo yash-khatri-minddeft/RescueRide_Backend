b24a61f591065e6df2886c37ddcdbb13a1a163e3
ParthMinddeft <parth.talsaniya@minddeft.net>
2023-05-31 14:12:43 +0530

    delete

--- controllers/adminCtrl.js
+++ controllers/adminCtrl.js
@@ -1,8 +1,8 @@
 const admin = require("./../models/AdminModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const otpGenerator = require('otp-generator');
-const { sendMail } = require('./MailController');
+const otpGenerator = require("otp-generator");
+const { sendMail } = require("./MailController");
 
 const adminRegisterController = async (req, res) => {
   try {
@@ -44,27 +44,34 @@ const adminLoginController = async (req, res) => {
         .send({ message: "Invalid Email Or Password", success: false });
     }
     req.session.user = user._id;
-    req.session.OTP = otpGenerator.generate(6, { upperCaseAlphabets: false, specialChars: false, lowerCaseAlphabets: false });
-    console.log(req.session.OTP)
+    req.session.OTP = otpGenerator.generate(6, {
+      upperCaseAlphabets: false,
+      specialChars: false,
+      lowerCaseAlphabets: false,
+    });
+    console.log(req.session.OTP);
     var maskedid = "";
     var myemailId = req.body.email;
     var index = myemailId.lastIndexOf("@");
     var prefix = myemailId.substring(0, index);
     var postfix = myemailId.substring(index);
 
-    var mask = prefix.split('').map(function (o, i) {
-      if (i < 2 || i >= (index - 2)) {
-        return o;
-      } else {
-        return '*';
-      }
-    }).join('');
+    var mask = prefix
+      .split("")
+      .map(function (o, i) {
+        if (i < 2 || i >= index - 2) {
+          return o;
+        } else {
+          return "*";
+        }
+      })
+      .join("");
 
     maskedid = mask + postfix;
     res
       .status(200)
       .send({ message: `OTP sent succesfully to ${maskedid}`, success: true });
-    sendMail(req.body.email, req.session.OTP)
+    sendMail(req.body.email, req.session.OTP);
   } catch (error) {
     console.log(error);
     res.status(500).send({
@@ -77,12 +84,18 @@ const adminOTPController = async (req, res) => {
   try {
     const { otp } = req.body;
     if (otp == req.session.OTP) {
-      const token = jwt.sign({ id: req.session.user }, process.env.JWT_SECRETKEY, {
-        expiresIn: "7d",
-      });
-      res.status(200).send({ message: "Login Successfully", success: true, token })
+      const token = jwt.sign(
+        { id: req.session.user },
+        process.env.JWT_SECRETKEY,
+        {
+          expiresIn: "7d",
+        }
+      );
+      res
+        .status(200)
+        .send({ message: "Login Successfully", success: true, token });
     } else {
-      res.status(200).send({ message: "OTP invalid", success: false })
+      res.status(200).send({ message: "OTP invalid", success: false });
     }
   } catch (err) {
     console.log(err);
@@ -90,7 +103,7 @@ const adminOTPController = async (req, res) => {
       message: `Error in Login ${err.message}`,
     });
   }
-}
+};
 
 const authController = async (req, res) => {
   try {
@@ -114,19 +127,23 @@ const authController = async (req, res) => {
 
 const checkAdminLogin = async (req, res) => {
   const getUserDetail = await admin.findById({ _id: req.body.userId });
-  if(getUserDetail) {
-    res.status(200).send({ message: 'User Logged In', isAdmin: true, userId: req.body.userId })
+  if (getUserDetail) {
+    res
+      .status(200)
+      .send({
+        message: "User Logged In",
+        isAdmin: true,
+        userId: req.body.userId,
+      });
   } else {
-    res.status(200).send({message: 'User not found', isAdmin: false})
+    res.status(200).send({ message: "User not found", isAdmin: false });
   }
-}
-
-
+};
 
 module.exports = {
   adminRegisterController,
   adminLoginController,
   adminOTPController,
   authController,
-  checkAdminLogin
+  checkAdminLogin,
 };
