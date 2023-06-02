b3d33aefde361d594be44a086b2510a4f6636a22
ParthMinddeft <parth.talsaniya@minddeft.net>
2023-06-02 15:19:46 +0530

    Driver signin and otp

--- routes/DriverRoutes.js
+++ routes/DriverRoutes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const AuthMiddleWare = require('../middlewares/AuthMiddleWare');
-const { checkLogin, authController } = require('../controllers/driverCtrl');
+const { checkLogin, authController,driverLoginController,driverOTPController } = require('../controllers/driverCtrl');
 const router = express.Router();
 
 router.get('/check-login', AuthMiddleWare, checkLogin)
 
 router.get('/getUserData', AuthMiddleWare, authController);
+
+router.post('/driver-login',driverLoginController)
+
+router.post('/driver-otp',driverOTPController);
 
 module.exports = router;
