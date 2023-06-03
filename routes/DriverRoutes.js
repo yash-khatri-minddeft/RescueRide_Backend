f0196e0e513dc45b4c14c8fa3a2cb6ae2d6def41
Yash Khatri <yash.khatri@minddeft.net>
2023-06-03 17:58:00 +0530

    driver controller

--- routes/DriverRoutes.js
+++ routes/DriverRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const AuthMiddleWare = require('../middlewares/AuthMiddleWare');
-const { checkLogin, authController,driverLoginController,driverOTPController, updateAmbulanceLocation } = require('../controllers/driverCtrl');
+const { checkLogin, authController,driverLoginController,driverOTPController, updateAmbulanceLocation, accpetBookingRequest } = require('../controllers/driverCtrl');
 const router = express.Router();
 
 router.get('/check-login', AuthMiddleWare, checkLogin)
@@ -12,5 +12,7 @@ router.post('/driver-login',driverLoginController)
 router.post('/driver-otp',driverOTPController);
 
 router.post('/update-ambulance-location', AuthMiddleWare, updateAmbulanceLocation)
+
+router.post('/update-booking-status', AuthMiddleWare, accpetBookingRequest);
 
 module.exports = router;
