88fd2e51a9e7b0ec53881cf74e9930afc87883db
Yash Khatri <yash.khatri@minddeft.net>
2023-06-05 18:01:05 +0530

    render site

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const hospital = require('./models/HospitalModel');
 
 const io = new Server({
   cors: {
-    origin: "http://localhost:5173"
+    origin: "https://rescueride.onrender.com/"
   }
 })
 
